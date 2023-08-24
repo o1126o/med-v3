440c046c088993b264fc581a27d66fa320c54d23
pmx <2606128175@qq.com>
2023-08-24 15:00:44 +0800

    feat(global): 仓库状态和vant及移动端适配

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,12 @@
+// createRouter 创建路由实例，===> new VueRouter()
+// history 是路由模式，hash模式，history模式
+// createWebHistory() 是开启history模块
+// createWebHashHistory() 是开启hash模式
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
+  //  import.meta.env.BASE_URL 是路由的基准地址，默认是 '/'
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
---
+++ src/types/user.d.ts
@@ -0,0 +1,8 @@
+// 用户信息
+export type User = {
+  token: string
+  id: string
+  account: string //⽤户名称
+  mobile: string
+  avatar: string
+}
